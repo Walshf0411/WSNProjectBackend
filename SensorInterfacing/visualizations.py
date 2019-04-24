e2a7e069301bf5f9d5a0c78d017e0c82287072d7
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2019-04-24 14:22:28 +0530

    Added comments to the code.

--- SensorInterfacing/visualizations.py
+++ SensorInterfacing/visualizations.py
@@ -10,23 +10,28 @@ from WSN import settings
 # generate sizes for the makers
 s = [20*4**4 for n in range(settings.NUMBER_OF_NODES)]
 
+# difference between two nodes
 difference = 2
-max_temp = None
-min_temp = None
-temp = None
-avg_temp = None
-threshold = 22.5
+max_temp = None # this will be max_temprature_of_dataset + 2
+min_temp = None # this will be min_temprature_of_dataset - 2
+temp = None # this is for the list of the nodes
+avg_temp = None # average temprature of the nodes
+threshold = 22.5 # threshold temprature will be the average temprature
 
 def get_key(coordinates):
+    # this method returns the key of supplied value from the nodes dictionary
     for key, node in settings.NODES.items():
         if node == coordinates:
             return key
     return None
 
 def get_temprature(node):
+    # This method returns the node temprature 
     if node[0] == 0 or node[1] == 0 or node[0] >= settings.NUMBER_OF_NODES or node[1] >= settings.NUMBER_OF_NODES:
+        # if the node is a boundary node, the temprature will be equal to average temprature
         return avg_temp
     else:
+        # if the node is not a boundary node, the temprature will be taken from the temprature dataset
         value = temp[get_key(node) - 1]
         if value:
             return value
@@ -34,9 +39,11 @@ def get_temprature(node):
             return avg_temp
 
 def normalize_temprature(temprature):
+    # method returns a temprature that is between 0 and 1 for using as alpha value in rgba()
     return (temprature - min_temp) / (max_temp - min_temp)
 
 def plot_individual_regions(set_of_nodes):
+    # plots the nodes supplied to it on the graph, basically it plots a region for the node
     if isinstance(set_of_nodes, dict):
         iterable = set_of_nodes.values()
     else:
@@ -70,6 +77,7 @@ def plot_individual_regions(set_of_nodes):
             # if it is not an instance of dict it means that it is one of the extra nodes
             print("Block " + str(i + 1 + settings.NUMBER_OF_NODES) + ": " +str(temprature_block))
 
+        # check if temprature is lower or below the threshold, to determine color of the block
         if temprature_block > threshold:
             plt.scatter([avg_x, ], [avg_y], c=[(1, 0, 0, normalize_temprature(temprature_block)), ], marker='s', s=s)
         else:
@@ -83,6 +91,7 @@ def do_visualization(temprature_data):
     
     threshold = avg_temp
 
+    # for ensuring no value is 0 or 1, as 0 gives a blank block and 1 gives a very dark blokc
     min_temp = min(temprature_data) - 2
     max_temp = max(temprature_data) + 2
 
@@ -98,7 +107,9 @@ def do_visualization(temprature_data):
 
     plt.scatter(settings.x_coordinates, settings.y_coordinates, c='blue', label="Temperature sensors")
     #plt.grid()
+    # plotting the entrance
     plt.plot([7, 8, 7, 8, 7, 7], [1.5, 1.5, 1, 1, 1.5, 1], label='Entrance', color='g')
+    # plotting the legend
     plt.legend(handles=[
         Patch(facecolor=(1, 0, 0, 1), edgecolor='r', label='Temperature > '+str(threshold)),
         Patch(facecolor=(0, 0, 1, 1), edgecolor='b', label='Temperature < ' + str(threshold)), 
